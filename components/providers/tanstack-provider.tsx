ce13dd76aed44405912a6cd6f9afced51de76b9a
nammtrong023 <ttnamm23@gmail.com>
2024-01-02 00:25:46 +0700

    Final commit

--- components/providers/tanstack-provider.tsx
+++ components/providers/tanstack-provider.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
 const TanStackProvider = ({ children }: { children: React.ReactNode }) => {
     return (
         <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={true} />
             {children}
         </QueryClientProvider>
     );
